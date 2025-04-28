0ce6cc7e6189992683044c3d94062bfa1e26228d
Bowie <santoscsrita@gmail.com>
2025-04-28 20:30:26 -0300

    nova função cliqueiNaMascara que altera a visibilidade da mascara e localidade do formulário

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
-const form = document.querySelector("formulario-fale-conosco")
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
 
 function cliqueiNoBotao() {
     form.style.left = "50%"
     form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+}
+function cliqueiNaMascara(){
+    mascara.style.visibility = "hidden"
+    form.style.left = "-350px"
+    form.style.transform = "translateX(0)"
 }
